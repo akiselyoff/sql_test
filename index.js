8f8f5df992de355c859d0a43601abf52e63b4425
kesel <a.kiselyoff@gmail.com>
2023-03-02 00:01:01 +0200

    simple server get for sql query

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
+const express = require('express');
 const mysql = require('mysql');
-const pool = mysql.createPool({
-  connectionLimit: 10,
+
+const app = express();
+
+const db = mysql.createConnection({
   host: 'happymak.mysql.tools',
   user: 'happymak_petsearch',
   password: ';2xK94Us(k',
   database: 'happymak_petsearch',
 });
 
-pool.getConnection((err, connection) => {
+//Connect
+db.connect(err => {
   if (err) throw error;
-  const query = 'CREATE TABLE testPool(Id INT,Age INT,FirstName VARCHAR(20),LastName VARCHAR(20))';
+  console.log('MySQL connected...');
+});
 
-  connection.query(query, function (err, results, fields) {
-    console.log(results);
-    connection.release();
-    if (err) {
-      connection.destroy();
-      throw error;
-    }
-  });
+//Create DB
+//Use when databse option in createConnection was absent
+
+// app.get('/createdb', (req, res) => {
+//   let sql = 'CREATE DATABASE TestMySQL';
+
+//   db.query(sql, (err, results) => {
+//     if (err) {
+//       throw error;
+//     }
+//     console.log(results);
+//     res.send('Database created...');
+//   });
+// });
+
+// app.get('/create_table', (req, res) => {
+//   let sql =
+//     'CREATE TABLE users(id INT AUTO_INCREMENT PRIMARY_KEY, firstname VARCHAR(30), lastname VARCHAR(30), age INT';
+
+//   db.query(sql, (err, results) => {
+//     if (err) {
+//       throw error;
+//     }
+//     console.log(results);
+//     res.send('Table created...');
+//   });
+// });
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server was started on port ${PORT}...`);
 });
