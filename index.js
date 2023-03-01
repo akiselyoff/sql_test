f0fc6c5582deca219ed997782af5f5444e5897a4
kesel <a.kiselyoff@gmail.com>
2023-03-01 12:01:36 +0200

    add mysql npm and testing it

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hi Jack!');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'happymak.mysql.tools',
+  user: 'happymak_petsearch',
+  password: ';2xK94Us(k',
+  database: 'happymak_petsearch',
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end(err =>
+  err ? console.log(`Connection end with error: ${err}`) : console.log(`Connection successfully end`)
+);
