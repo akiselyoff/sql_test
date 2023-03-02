e4277e4b36b60fcb9bb6ac1d63ff7451f3eb97d6
kesel <a.kiselyoff@gmail.com>
2023-03-02 12:49:50 +0200

    rebase secret vars to env

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
 const mysql = require('mysql');
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+const { PORT = 3000, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 const db = mysql.createConnection({
-  host: 'happymak.mysql.tools',
-  user: 'happymak_petsearch',
-  password: ';2xK94Us(k',
-  database: 'happymak_petsearch',
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
 
 //Connect
@@ -31,20 +38,19 @@ db.connect(err => {
 //   });
 // });
 
-// app.get('/create_table', (req, res) => {
-//   let sql =
-//     'CREATE TABLE users(id INT AUTO_INCREMENT PRIMARY_KEY, firstname VARCHAR(30), lastname VARCHAR(30), age INT';
+app.get('/create_table', (req, res) => {
+  let sql =
+    'CREATE TABLE users(id INT AUTO_INCREMENT PRIMARY_KEY, firstname VARCHAR(30), lastname VARCHAR(30), age INT';
 
-//   db.query(sql, (err, results) => {
-//     if (err) {
-//       throw error;
-//     }
-//     console.log(results);
-//     res.send('Table created...');
-//   });
-// });
+  db.query(sql, (err, results) => {
+    if (err) {
+      throw error;
+    }
+    console.log(results);
+    res.send('Table created...');
+  });
+});
 
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server was started on port ${PORT}...`);
 });
